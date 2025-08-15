be620f14869f0c283f65f1e27e20b3def90ded97
Tayfun çakır <tayfun.cakirr9@gmail.com>
2025-08-15 19:46:16 +0300

    react-router-dom, usenavigate ve useparams kullanımı

--- 7.routing/src/App.jsx
+++ 7.routing/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Users from './pages/Users'
 import Contact from './pages/Contact'
 import Menu from "./components/Menu"
+import UserDetail from "./pages/UserDetail"
 
 function App() {
  
@@ -15,6 +16,7 @@ function App() {
      <Routes>
        <Route index element={<Home/>} />
        <Route path='users' element={<Users/>}/>
+       <Route path='users/:id' element={<UserDetail/>}/>
        <Route path='contact' element={<Contact/>}/>
      </Routes>
    </BrowserRouter>
--- 7.routing/src/components/Menu.jsx
+++ 7.routing/src/components/Menu.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import "../App.css"
 
-
+//useNavigate ile Link bileşeni kullanılmadan da etiketin onclick özelliği ile de yönlendirme yapılabilir.
 function Menu() {
+    const navigate = useNavigate();
   return (
     <div>
         <ul className='menu'>
+            <li style={{cursor:'pointer'}} onClick={()=> navigate( -1)}>Geri</li>
             <li><Link to="/">Anasayfa</Link></li> 
             <li><Link to="users">Kullanıcılar</Link></li>
             <li><Link to="contact">İletişim</Link></li>
