aa8f9faf8d256ee67479f9cd997fb2e0a3b1c282
Tayfun çakır <tayfun.cakirr9@gmail.com>
2025-08-18 16:18:08 +0300

    todo-list ,create,delete ve localStorage ile local e kayıt

---
+++ todo-list-examples/src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState ([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(()=>{
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos){
+      setTodos(JSON.parse(savedTodos));
+    }
+  },[]);
+
+  const addTodo = () =>{
+    if(inputValue.trim() === "") return;
+    const newTodos =[...todos, inputValue];
+    setTodos(newTodos);
+    setInputValue("");
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  }
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_,i) => i !== index);
+    setTodos(updatedTodos);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  }
+
+  const deleteAll = () =>{
+    setTodos([]);
+    localStorage.removeItem("todos");
+  }
+
+  return (
+    <div className='list-container'>
+      <input
+        type="text"
+        placeholder='Todo Giriniz...'
+        value={inputValue}
+        onChange={(event)=> setInputValue(event.target.value.trim())}
+      />
+      <button className='add-btn' onClick={addTodo}>Ekle</button>
+      <ul className='ul-list'>
+        {todos.map((todo,index) =>(
+            <li key={index}>
+              {todo}
+              <button className='delete-todo-btn' onClick={()=> deleteTodo(index)}>x</button>
+              </li>
+        ))}
+      </ul>
+      <button className='delete-all-btn' onClick={deleteAll}>Tümünü Sil</button>
+      {todos.length === 0 && <p className='warning'>Henüz todo yok</p>}
+    </div>
+  )
+}
+
+export default App
